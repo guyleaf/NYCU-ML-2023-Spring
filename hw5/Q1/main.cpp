7ba6ecb7793b44c29d3fc238ab3bf0406546c262
guyleaf <leaf.ying.work@gmail.com>
2023-05-08 01:50:17 +0800

    Finish HW5 Q1

--- hw5/Q1/main.cpp
+++ hw5/Q1/main.cpp
@@ -7,6 +7,13 @@
 #include <cmath>
 #include <random>
 
+#include <imgui.h>
+#include <implot.h>
+#include <backends/imgui_impl_glfw.h>
+#include <backends/imgui_impl_opengl3.h>
+
+#include <GLFW/glfw3.h> // Will drag system OpenGL headers
+
 #define NUM_THREADS 12
 #define EIGEN_USE_MKL_ALL
 
@@ -25,13 +32,17 @@
 namespace fs = boost::filesystem;
 
 const std::string DATA_FILE = "input.data";
+const std::string TITLE = "ML HW5 Q1";
 
-constexpr double STOP_APPROXIMATION_THRESHOLD = 1e-7;
+constexpr ImVec4 COLOR_BLACK = ImVec4(0, 0, 0, 1);
+constexpr ImVec4 COLOR_RED = ImVec4(1, 0, 0, 1);
+constexpr ImVec4 COLOR_BLUE = ImVec4(0, 0, 1, 1);
 
 #pragma region Data Structures
 
 using MatrixXd = Eigen::MatrixXd;
 using MatrixX2d = Eigen::MatrixX2d;
+using MatrixX3d = Eigen::MatrixX3d;
 
 using VectorXd = Eigen::VectorXd;
 using Vector3d = Eigen::Vector3d;
@@ -41,6 +52,145 @@ struct LossArguments
     const MatrixX2d &data;
     double beta;
 };
+
+#pragma endregion
+
+#pragma region GUI Functions
+
+static void glfwErrorCallback(int error, const char *description)
+{
+    std::cerr << "GLFW Error " << error << ": " << description << std::endl;
+}
+
+GLFWwindow *setUpGUI()
+{
+    glfwSetErrorCallback(glfwErrorCallback);
+    if (!glfwInit())
+    {
+        return nullptr;
+    }
+
+    // Decide GL+GLSL versions
+    // GL 3.3 + GLSL 330
+    const char *glsl_version = "#version 330";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // 3.2+ only
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);           // 3.0+ only
+
+    // Create window with graphics context
+    GLFWwindow *window = glfwCreateWindow(1280, 720, TITLE.c_str(), nullptr, nullptr);
+    if (window == nullptr)
+        return nullptr;
+
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(0);
+
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImPlot::CreateContext();
+
+    ImGuiIO &io = ImGui::GetIO();
+
+    ImFontConfig cfg;
+    cfg.SizePixels = 15;
+    io.Fonts->AddFontDefault(&cfg);
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsLight();
+    // ImGui::StyleColorsDark();
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init(glsl_version);
+
+    return window;
+}
+
+void drawPlot(const std::string &title, const MatrixX2d &data, const MatrixX3d &f)
+{
+    if (ImPlot::BeginPlot(title.c_str()))
+    {
+        ImPlot::SetupAxes("x", "y");
+        ImPlot::SetupAxisLimits(ImAxis_X1, -60, 60);
+        // ImPlot::SetupAxisLimits(ImAxis_Y1, -60, 60);
+
+        ImPlot::SetNextMarkerStyle(ImPlotMarker_Circle, IMPLOT_AUTO, COLOR_RED, IMPLOT_AUTO, COLOR_RED);
+        ImPlot::PlotScatter("train data", data.col(0).data(), data.col(1).data(), data.rows());
+
+        VectorXd variance = 2 * f.col(2);
+        VectorXd upperBound = f.col(1) + variance;
+        VectorXd lowerBound = f.col(1) - variance;
+        ImPlot::SetNextFillStyle(COLOR_BLUE, 0.5f);
+        ImPlot::PlotShaded("f(x)'s 95%% confidence", f.col(0).data(), upperBound.data(), lowerBound.data(), f.rows());
+
+        ImPlot::SetNextLineStyle(COLOR_BLACK);
+        ImPlot::PlotLine("f(x)'s mean", f.col(0).data(), f.col(1).data(), f.rows());
+
+        ImPlot::EndPlot();
+    }
+}
+
+void showGUI(const MatrixX2d &data, const MatrixX3d &f, const MatrixX3d &optimizedF)
+{
+    auto window = setUpGUI();
+    if (window == nullptr)
+    {
+        throw std::runtime_error("Cannot create window.");
+    }
+
+    // Our state
+    auto clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+    // Main loop
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+
+        // Start the Dear ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        {
+            const auto windowSize = ImGui::GetIO().DisplaySize;
+
+            ImGui::SetNextWindowPos(ImVec2(0, 0), ImGuiCond_Always);
+            ImGui::SetNextWindowSize(ImVec2(windowSize.x, windowSize.y), ImGuiCond_Always);
+            ImGui::Begin("Result", nullptr, ImGuiWindowFlags_NoDecoration);
+
+            if (ImPlot::BeginSubplots("Result", 1, 2, ImVec2(windowSize.x, windowSize.y)))
+            {
+                drawPlot("Original", data, f);
+                drawPlot("Optimized", data, optimizedF);
+                ImPlot::EndSubplots();
+            }
+
+            ImGui::End();
+        }
+
+        // Rendering
+        ImGui::Render();
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        glfwSwapBuffers(window);
+    }
+
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImPlot::DestroyContext();
+    ImGui::DestroyContext();
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+}
 
 #pragma endregion
 
@@ -140,18 +290,31 @@ double evaluateOptimFn(const VectorXd &parameters, VectorXd *grad_out, void *arg
 
 #pragma region Custom Functions
 
-void drawPlot(const MatrixX2d &data, const MatrixXd &covariance, double beta, const Vector3d &kernelParameters)
+MatrixX3d generatePointsOfLine(const MatrixX2d &data, double beta, const MatrixXd &covariance, const Vector3d& kernelParameters)
 {
+    MatrixXd inv_covariance = covariance.inverse();
+    double inv_beta = 1 / beta;
+    MatrixX3d f(250, 3);
+    f.col(0) = VectorXd::LinSpaced(250, -60, 60);
+
+    for (auto row : f.rowwise())
+    {
+        double k = calculateRationalQuadraticKernel(row[0], row[0], kernelParameters) + inv_beta;
+        VectorXd kernel = calculateRationalQuadraticKernel(data.col(0), row[0], kernelParameters);
+
+        Eigen::Matrix<double, 1, Eigen::Dynamic> common = kernel.transpose() * inv_covariance;
+        row[1] = (common * data.col(1)).value();
+        row[2] = k - (common * kernel).value();
+    }
+
+    return f;
 }
 
 void modelData(const MatrixX2d &data, double beta)
 {
     // variance, alpha, length scale
     Vector3d kernelParameters = Vector3d::Constant(1);
-
     MatrixXd covariance = calculateCovariance(data, beta, kernelParameters);
-
-    std::cout << covariance << std::endl;
 
     LossArguments args(data, beta);
     VectorXd optimizedKernelParameters = kernelParameters;
@@ -168,8 +331,9 @@ void modelData(const MatrixX2d &data, double beta)
 
     MatrixXd optimizedCovariance = calculateCovariance(data, beta, optimizedKernelParameters);
 
-    std::cout << optimizedCovariance << std::endl << std::endl;
-    std::cout << optimizedKernelParameters << std::endl;
+    MatrixX3d f = generatePointsOfLine(data, beta, covariance, kernelParameters);
+    MatrixX3d optimizedF = generatePointsOfLine(data, beta, optimizedCovariance, optimizedKernelParameters);
+    showGUI(data, f, optimizedF);
 }
 
 #pragma endregion
